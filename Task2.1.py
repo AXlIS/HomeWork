81082fe4bd2f3ede9a542671f105a18ff641c9eb
BRODYAGA88 <61621218+BRODYAGA88@users.noreply.github.com>
2020-04-02 08:54:13 +0300

    Update Task2.1.py

--- Task2.1.py
+++ Task2.1.py
@@ -1,6 +1,6 @@
-my_list =[]
+my_list = []
 n = int(input('Введите колличество элементов списка: '))
-last_element = 0
+last_element = None
 
 for i in range(1, n + 1):
     my_list.append(int(input(f'Введите {i}-й элемент списка: ')))
@@ -10,12 +10,13 @@ print(my_list)
 if len(my_list) % 2 == 1:
     last_element = my_list.pop()
 
+print(last_element)
+
 for number, element in enumerate(my_list):
     if number % 2 == 0:
         my_list.insert(number + 2, my_list[number])
         my_list.remove(my_list[number])
 
-if last_element != 0:
- my_list.append(last_element)
+my_list.append(last_element)
 
 print(my_list)
