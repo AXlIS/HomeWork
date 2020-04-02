932894520a02b235b3004dadee9fcba2182afb56
BRODYAGA88 <61621218+BRODYAGA88@users.noreply.github.com>
2020-04-02 09:02:48 +0300

    Update Task6.1.py

--- Task6.1.py
+++ Task6.1.py
@@ -10,10 +10,8 @@ for i in range(1, n + 1):
        'колличество': int(input('Введите колличество: ')),
        'ед': input('Введите единицу измерения: ')})
     my_list.append(my_tuple)
-    analiz['товар'].append(my_tuple[1]['товар'])
-    analiz['цена'].append(my_tuple[1]['цена'])
-    analiz['колличество'].append(my_tuple[1]['колличество'])
-    analiz['ед'].append(my_tuple[1]['ед'])
+    for i in analiz:
+        analiz[str(i)].append(my_tuple[1][str(i)])
 
 for i in range(len(my_list)):
     print(my_list[i])
