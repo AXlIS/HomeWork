2dd5368c5dac07172a73ede2f7309ff63a0afb5e
Igor Svistun <brodyaga88@LAPTOP-B8ANLTD9.localdomain>
2020-04-06 13:47:04 +0300

    HM

--- Task5.1.py
+++ Task5.1.py
@@ -2,6 +2,6 @@ n = int(input('Ведите колличество чисел: '))
 my_list = []
 for i in range(1, n + 1):
     my_list.append(int(input(f'Ведите {i}-е число:')))
-    my_list.sort(reverse= True)
+    my_list.sort(reverse=True)
 
 print(my_list)
